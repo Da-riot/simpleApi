2cc9308ac79368578ad7e95a1946e1531236b672
Da-riot <61190370+Da-riot@users.noreply.github.com>
2022-06-30 10:55:34 -0500

    Revert "register backend"

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const User = require('../models/user')
 const Joi = require('@hapi/joi');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken')
 
 
 
@@ -11,7 +12,11 @@ const schemaRegister = Joi.object({
     email: Joi.string().min(6).max(255).required().email(),
     password: Joi.string().min(6).max(1024).required()
 })
-
+// Esquema del login
+const schemaLogin = Joi.object({
+    email: Joi.string().min(6).max(255).required().email(),
+    password: Joi.string().min(6).max(1024).required()
+})
 
 router.post('/register', async (req, res) => {
     // Dentro del método que invoca POST
@@ -52,5 +57,31 @@ router.post('/register', async (req, res) => {
     }
 })
 
+// LOGIN
+router.post('/login', async (req, res) => {
+    // Validaciones de login
+    const { error } = schemaLogin.validate(req.body)
+    if(error) return res.status(400).json({error: error.details[0].message})
+
+    // Validaciond e existencia
+    const user = await User.findOne({email: req.body.email})
+    if(!user) return res.status(400).json({error: 'Usuario no encontrado'})
+
+    // Validacion de password en la base de datos
+    const validPassword = await bcrypt.compare(req.body.password, user.password)
+    if(!validPassword) return res.status(400).json({error: 'Constraseña invalida'})
+    // Creando token
+    const token = jwt.sign({
+        name: user.name,
+        id: user._id
+    }, process.env.TOKEN_SECRET)
+
+// Colocando el token en el header y el cuerpo de la respuesta
+    res.header('auth-token', token).json({
+        error: null,
+        data: { token },
+        message: 'Bienvenido'
+    })
+})
 
 module.exports = router
